961e2bd2cf71e524a67315e0e9eaaa57e227f80c
Pashan <pashanirani@gmail.com>
2017-03-13 23:30:31 -0700

    conflicts resolved

--- new-tab-clock-script.js
+++ new-tab-clock-script.js
@@ -1,17 +1,20 @@
 var timeDisplay = document.getElementById('timeDisplay');
 
-function currentTime(){
+function currentTime() {
 
-  var today = new Date();
-  var currentHours = today.getHours();
-  var currentMinutes = today.getMinutes();
-  var currentSeconds = today.getSeconds();
-  currentMinutes = checkTime(currentMinutes);
-  currentSeconds = checkTime(currentSeconds);
-  timeDisplay.innerHTML = currentHours + ':' + currentMinutes + ':' + currentSeconds;
-  var timeOut = setTimeout(currentTime, 500);
+    var today = new Date();
+    var currentHours = today.getHours();
+    var currentMinutes = today.getMinutes();
+    var currentSeconds = today.getSeconds();
+    currentMinutes = checkTime(currentMinutes);
+    currentSeconds = checkTime(currentSeconds);
+    timeDisplay.innerHTML = currentHours + ':' + currentMinutes + ':' + currentSeconds;
+    var timeOut = setTimeout(currentTime, 500);
 }
-function checkTime(i){
-  if (i < 10){i = '0' + i};
-  return i;
+
+function checkTime(i) {
+    if (i < 10) {
+        i = '0' + i;
+    }
+    return i;
 }
