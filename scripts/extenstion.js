1a42af156136dcd728c17c52c1b8789b6bf0086f
Pashan <pashanirani@gmail.com>
2017-03-13 22:49:20 -0700

    added extentsion scripts

---
+++ scripts/extenstion.js
@@ -0,0 +1,7 @@
+//Don't mess with this greg!
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({'url': chrome.extension.getURL('index.html')}, function(tab) {
+    // Tab opened.
+  });
+});
